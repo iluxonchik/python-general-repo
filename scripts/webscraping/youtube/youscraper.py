4c7ceb2489585f6b9fb528eac2d77189f53c84b6
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-12-19 16:29:40 +0000

    Add inital structure to youscraper

---
+++ scripts/webscraping/youtube/youscraper.py
@@ -0,0 +1,32 @@
+# Scrape YouTube video title, description and keywords
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import sys, io, argparse
+
+def getVideoTitle(bsObj):
+	"""
+	Returns the video title
+	"""
+	# TODO
+
+def getVideoDescription(bsObj):
+	"""
+	Returns the video description
+	"""
+	# TODO
+
+def getVideoTags(bsObj):
+	"""
+	Returns a list of video tags
+	"""
+	# TODO
+
+def getVideoLinks(url):
+	"""
+	Returns a list of videos from a page
+	"""
+	# TODO
+
+if __name__  == '__main__':
+	# TODO
