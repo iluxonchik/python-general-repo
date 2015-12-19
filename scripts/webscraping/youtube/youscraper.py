54fd6fb592c7cd12449d58ae58e4133471f60290
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-12-19 17:20:13 +0000

    Add args structure

--- scripts/webscraping/youtube/youscraper.py
+++ scripts/webscraping/youtube/youscraper.py
@@ -1,5 +1,5 @@
 # Scrape YouTube video title, description and keywords
-
+# TODO: organize the code in classes
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import sys, io, argparse
@@ -15,12 +15,14 @@ def getVideoDescription(bsObj):
 	Returns the video description
 	"""
 	# TODO
+	# <meta name="description" 
 
 def getVideoTags(bsObj):
 	"""
 	Returns a list of video tags
 	"""
 	# TODO
+	# <meta name="keywords" 
 
 def getVideoLinks(url):
 	"""
@@ -28,5 +30,21 @@ def getVideoLinks(url):
 	"""
 	# TODO
 
+
+def positive_check(value:str):
+	"""
+	Check if the provided string is a positive integer
+	"""
+	val = int(value)
+	if val > 0:
+		return val
+	raise argparse.ArgumentTypeError('{} is not a postivie interger'.format(value))
+
 if __name__  == '__main__':
 	# TODO
+	parser = argparse.ArgumentParser(description='Scrapes YouTube video info')
+	parser.add_argument('search', help='search url or search query', type=str)
+	parser.add_argument('-d', '--domain', help='search domain ("com", "co.uk", "ru", "com.br", etc) default is "com"', type=str, default="com")
+	parser.add_argument('-o', '--out', help='output file name', dest='filename', type=str)
+	parser.add_argument('-p', '--pages', help='number of pages to parse (starting from page 1)', type=positive_check, default=1)
+	args = parser.parse_args()
