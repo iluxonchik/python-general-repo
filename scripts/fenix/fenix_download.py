10e9a3e9c5a8e08519487b8c3a4e53e06f69931a
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-12-26 23:59:48 +0000

    Download script additions

--- scripts/fenix/fenix_download.py
+++ scripts/fenix/fenix_download.py
@@ -3,12 +3,34 @@
 #
 
 from fenix import Fenix
-import argparse
+from exceptions import LoginFailedException
+from bs4 import BeautifulSoup
+import json, argparse, os
+from urllib.request import urlretrieve
+
+def parse_user(file_name):
+    """
+    Parse user credentials from the json file
+    """    
+    with open(file_name, 'r') as f:
+        return json.load(f)["user"]
+
+def main(url, login_file, file_ext, dl_dir):
+    user = parse_user(login_file) # get login credentials
+    fenix = Fenix(user["username"], user["password"]).login()
+
+    if fenix is None:
+        raise LoginFailedException('Could not log in, please make sure that the provided credentials ' +
+                                 'are correct.' )
+
 
 if __name__ == '__main__':
     # TODO: argparse
     parser = argparse.ArgumentParser(description='Download files from Fenix Edu pages')
-    parser.add_argument('login', type=str, help='path to fenix login credentials file (check login.json for format)')
     parser.add_argument('url', type=str, help='url from where to download the files from')
-    parser.add('-e', '--extension', type=str, default='pdf', help='file extensions to download, without the leading "." (* for all, default is "pdf")')
-    parser.add('-d', '--directory', type=str, default='download', help='download directory')
+    parser.add_argument('-l', '--login', type=str, default="login.json" , help='path to fenix login credentials file (check login.json for format)')
+    parser.add_argument('-e', '--extension', type=str, default='pdf', help='file extensions to download, without the leading "." (* for all, default is "pdf")')
+    parser.add_argument('-d', '--directory', type=str, default='download', help='download directory')
+    args = parser.parse_args()
+
+    main(args.url, args.login, args.extension, args.directory)
