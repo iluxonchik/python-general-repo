704d8e3595979b490c246bb76d8e7e2a523b0153
ILUXONCHIK <iluxon4ik@hotmail.com>
2015-12-27 00:42:09 +0000

    fenix_downloader.py now downloads files from the provided url

--- scripts/fenix/fenix_download.py
+++ scripts/fenix/fenix_download.py
@@ -5,7 +5,7 @@
 from fenix import Fenix
 from exceptions import LoginFailedException
 from bs4 import BeautifulSoup
-import json, argparse, os
+import json, argparse, os, re
 from urllib.request import urlretrieve
 
 def parse_user(file_name):
@@ -15,6 +15,20 @@ def parse_user(file_name):
     with open(file_name, 'r') as f:
         return json.load(f)["user"]
 
+def get_download_path(dl_dir, file_url):
+    if not os.path.exists(dl_dir):
+        os.makedirs(dl_dir)
+
+    # get the last part of url as file_name
+    file_name = file_url.split("/")[-1]
+    return dl_dir + '/' + file_name
+
+# TODO: this should be a method in Fenix API
+def download_file(fenix, url, file):
+    with open(file, 'wb') as f:
+        f.write(fenix.open(url).read())
+
+
 def main(url, login_file, file_ext, dl_dir):
     user = parse_user(login_file) # get login credentials
     fenix = Fenix(user["username"], user["password"]).login()
@@ -22,7 +36,17 @@ def main(url, login_file, file_ext, dl_dir):
     if fenix is None:
         raise LoginFailedException('Could not log in, please make sure that the provided credentials ' +
                                  'are correct.' )
-
+    # TODO: exception handling
+    html = fenix.open(url)
+    bsObj = BeautifulSoup(html, 'html.parser')
+    content_div = bsObj.find('div', {'class':'col-sm-9 main-content'})
+    # maybe trust internal links only? add an opt to argparse
+    anchors = content_div.findAll('a', {'href':re.compile('^(http)(.)*(.' + file_ext + ')$')})
+    
+    for anchor in anchors:
+        dl_path = get_download_path(dl_dir, anchor['href'])
+        print('Downloading ' + dl_path + " ...")
+        download_file(fenix, anchor['href'], dl_path)
 
 if __name__ == '__main__':
     # TODO: argparse
